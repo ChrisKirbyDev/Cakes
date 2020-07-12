d9067e489a40cc966f8e501d408c2454c6dd377f
Chris <chriskirbydev@gmail.com>
2020-07-13 11:14:26 +1200

    set up file-loader in webpack to allow react to render images

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader",]
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif)$/i,
+        use: "file-loader"
       }
     ]
   },
