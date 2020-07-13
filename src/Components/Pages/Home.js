d585d9e97cdacece38a3b402cba974c68ab6de70
Chris <chriskirbydev@gmail.com>
2020-07-13 13:16:16 +1200

    Add header component to home and app.js

---
+++ src/Components/Pages/Home.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import Header from "../Common/Header";
+
+// Re-usable components
+
+class Home extends Component {
+    render(){
+        return (
+            <div>
+                <Header/>
+            </div>
+        )
+    }
+}
+
+export default Home;
