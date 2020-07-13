2c4c7986fb309d73b82a1ef80350a15406ef3391
Chris <chriskirbydev@gmail.com>
2020-07-13 14:05:49 +1200

    Failing to compile storybook, removed hard path for import react, issue resolved

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "./node_modules/react";
+import React, {Component} from "react";
 import {hot} from "react-hot-loader";
 import Navbar from "./navbar";
 import "../styles/styles.css";
--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,4 +1,4 @@
-import React, {Component} from "./node_modules/react";
+import React, {Component} from "react";
 import Header from "../common/header";
 
 // Re-usable components
