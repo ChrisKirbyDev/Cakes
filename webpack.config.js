41e760f186e5e5ee31375de6dd5094354e04a010
Chris <chriskirbydev@gmail.com>
2020-07-13 15:12:49 +1200

    import logo and set text-alignment to centre

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader",]
+        use: ["style-loader", "css-loader",],
+        options: true
       },
       {
         test: /\.(png|jpe?g|svg|gif)$/i,
