bef0faa1fe83dc7436e3d0929ac998b056003f4c
Chris <chriskirbydev@gmail.com>
2020-07-15 10:39:08 +1200

    resize portfolio image height, create about section

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -3,12 +3,14 @@ import React from "react";
 // Re-usable components
 import Header from "../common/header";
 import Portfolio from "../common/portfolio";
+import About from "../common/about";
 
 function Home(){
         return (
             <div>
                 <Header />
                 <Portfolio />
+                <About />
             </div>            
         )
 }
