979a652f9418a4eb79ecb3fe91912af739a6cc39
Chris <chriskirbydev@gmail.com>
2020-07-16 14:51:47 +1200

    change title text, restyle button

--- src/Components/Common/Header.js
+++ src/Components/Common/Header.js
@@ -1,17 +1,19 @@
 import React from "react";
 import Image from "../assets/img/header-image.png";
 
+
 function Header(){
         return(
             <header className="header-section">
                 <img className="header-image" src={Image} alt="Cakes By Emily Eve Header"/>
                 <div className="header-content">
-                    <h1 className="header-title">Cakes By Emily Eve</h1>
+                    <h1 className="header-title">Life Is Sweet.</h1>
                     <h2 className="header-subtitle">Experience Love At First Bite!</h2>
-                    <button className="header-btn">Book A Consultation</button>
+                    <button className="header-btn btn-1">Book A Consultation</button>
                 </div>
             </header>
         )
 }
 
 export default Header;
+
