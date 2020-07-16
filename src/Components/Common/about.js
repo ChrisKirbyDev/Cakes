a1ae55ffc353697af6c8bd35d33520d60b336311
Chris <chriskirbydev@gmail.com>
2020-07-16 14:05:19 +1200

    restyle about section, place titles in a div to rearrange positioning easier.

--- src/Components/Common/about.js
+++ src/Components/Common/about.js
@@ -4,8 +4,10 @@ import Image from "../assets/img/about.png";
 function About(){
     return(
         <div className="about-section"> 
-            <h2>The First Cake I Made Was For Others.</h2>
-            <h3>Ever since, i've been making cakes <strong>styled for everyone.</strong></h3>
+            <div className="about-titles">
+                <h2>The First Cake I Made Was For Others.</h2>
+                <h3>Since then, <strong>nothing has changed.</strong></h3>
+            </div>            
             <div className="about-image">
                 <img className="profile-image" src={Image} alt="About Me Profile"/>
             </div>
@@ -14,11 +16,17 @@ function About(){
                 <p>I have grown up watching my Mum create wonderful masterpieces for mine and my sisters birthdays 
                 over the years and I couldn’t help but want to get involved,
                 making the odd thing out of icing for her to add to her creations every now and then.</p>
+                <br/>
                 <p>After showing my friends at college these little additions to my Mums creations,
                 a friend dared me to make him a birthday cake and bring it into college the next day.
                 So off I went, stopping off at the cake supply shop and supermarket on my way home.
                 I spent all night baking and decorating a cake,
                 using every utensil I could find and making an absolute mess in the process!</p>
+                <br/>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet porttitor neque.
+                Proin sit amet velit non lectus ultricies fringilla eu pellentesque metus. Pellentesque in est massa.
+                Morbi in dignissim nisi. Proin tincidunt tincidunt tortor et pellentesque. In ullamcorper suscipit ante ut sagittis.
+                Cras suscipit augue vitae consectetur porttitor. Sed pretium mi a sapien commodo tristique sed nec est.</p>
             </div>
         </div>
     )
