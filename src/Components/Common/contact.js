14e5e51d376b81852fa1b73155ed9cdc9bed6253
Chris <chriskirbydev@gmail.com>
2020-07-28 11:49:37 +1200

    add email to contact icons

--- src/Components/Common/contact.js
+++ src/Components/Common/contact.js
@@ -1,22 +1,28 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faYoutube,
-  faFacebook,
-  faTwitter,
-  faInstagram
+  faFacebook,  
 } from "@fortawesome/free-brands-svg-icons";
+import {  
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Contact(){
   const currentYear = new Date().getFullYear();
   return (
-    <footer>
+    <footer className="contact">
         <div className="contact-container">
           <a 
             href="https://www.facebook.com/cakesbyemilyeve"
             className="facebook"
           >
             <FontAwesomeIcon icon={faFacebook} size="2x" />
+          </a>
+          <a 
+            href="mailto:cakesbyemilyeve@gmail.com"
+            className="email"
+          >
+            <FontAwesomeIcon icon={faEnvelope} size="2x" />
           </a>
           <p>Copyright ⓒ {currentYear}</p>
         </div>
