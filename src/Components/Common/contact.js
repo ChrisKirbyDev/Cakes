29a3c8ab07efd7c98db9f1c301873cc7e48b339c
Chris <chriskirbydev@gmail.com>
2020-07-28 11:20:55 +1200

    add social media icons, install @fortawesome to dependencies

--- src/Components/Common/contact.js
+++ src/Components/Common/contact.js
@@ -1,13 +1,26 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram
+} from "@fortawesome/free-solid-svg-icons";
 
 function Contact(){
   const currentYear = new Date().getFullYear();
   return (
     <footer>
         <div className="contact-container">
-          
+          <a 
+            href="https://www.facebook.com/cakesbyemilyeve"
+            className="facebook"
+          >
+            <FontAwesomeIcon icon={faFacebook} size="2x" />
+          </a>
+          <p>Copyright ⓒ {currentYear}</p>
         </div>
-        <p>Copyright ⓒ {currentYear}</p>
+        
     </footer>
   )
 }
