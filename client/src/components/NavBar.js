929c0bd0d062378d2e19e438a1b4580db5dcf8bd
malyz <mattuy001@gmail.com>
2020-08-18 18:28:59 +1200

    fixed onclick btn

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,35 +4,42 @@ import { Button } from "./common/Button";
 import { Link } from "react-router-dom";
 
 class Navbar extends Component {
-    state = { clicked: false }
+  state = {
+    open: false
+  };
 
-    handleClick = () => {
-        this.setState({ clicked: !this.state.clicked })
-    }
+  handleOpen = () => {
+    this.setState({ open: !this.state.open });
+  };
 
-    render(){
-        return(            
-                <nav className="navbar" i="mainNav">
-                    <Link to="/"><img className="navbar-logo" src="/img/cbee_logo.svg" alt="website logo" /></Link>                    
-                        <div className="menu-icon" onClick={this.handleClick}>
-                            <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
-                        </div>
-                        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
-                            {MenuItems.map((item, index) => {
-                                return(
-                                    <li key={index}>
-                                        <a className={item.cName} href={item.url}>
-                                        {item.title}
-                                        </a>
-                                    </li>
-                                )
-                            })}                                                        
-                        </ul>
-                        <Button>Book</Button>
-                </nav>
-            
-        );
-    }
+  render() {
+    return (
+      <nav className="navbar" i="mainNav">
+        <Link to="/">
+          <img
+            className="navbar-logo"
+            src="/img/cbee_logo.svg"
+            alt="website logo"
+          />
+        </Link>
+        <div className="menu-icon" onClick={this.handleOpen}>
+          <i className={this.state.open ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
+        <ul className={this.state.open ? "nav-menu active" : "nav-menu"}>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index} onClick={this.handleOpen}>
+                <a className={item.cName} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <Button>Book</Button>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
