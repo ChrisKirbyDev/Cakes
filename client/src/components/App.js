e4917c667e82181900c68b9f5d2cc68fce0ee2eb
malyz <mattuy001@gmail.com>
2020-08-18 18:19:14 +1200

    Deleted unused imports

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/styles.css";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 // Pages
 import Home from "./pages/Home";
@@ -8,21 +8,17 @@ import Form from "./common/Form";
 import Navbar from "./NavBar";
 import Contact from "./common/Contact";
 
-function App(){  
-    return (
-      <Router>
+function App() {
+  return (
+    <Router>
       <div>
-       <Navbar />
-       <Route
-          path="/"
-          exact component={Home} />
-        <Route
-          path="/form"
-          exact component={Form} />
+        <Navbar />
+        <Route path="/" exact component={Home} />
+        <Route path="/form" exact component={Form} />
         <Contact />
       </div>
-      </Router>
-    )
+    </Router>
+  );
 }
 
 export default App;
