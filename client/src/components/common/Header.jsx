ab967239c76d983ef61718527051b13933404aad
Chris <chriskirbydev@gmail.com>
2020-08-19 13:53:08 +1200

    removed link components

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -1,19 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
-
-function Header(){
-        return(
-            <header className="header-section" id="header">
-                <img className="header-image" src="/img/header-image.png" alt="Cakes By Emily Eve Header"/>
-                <div className="header-content">
-                    <h1 className="header-title">Life Is Sweet.</h1>
-                    <h2 className="header-subtitle">Experience Love At First Bite!</h2>
-                    <Link to="/form"><button className="header-btn btn-1">Book A Consultation</button></Link>
-                </div>
-            </header>
-        )
+function Header() {
+  return (
+    <header className="header-section" id="header">
+      <img
+        className="header-image"
+        src="/img/header-image.png"
+        alt="Cakes By Emily Eve Header"
+      />
+      <div className="header-content">
+        <h1 className="header-title">Life Is Sweet.</h1>
+        <h2 className="header-subtitle">Experience Love At First Bite!</h2>
+        <div onClick={() => this.props.history.push("/form")}>
+          <button className="header-btn btn-1">Book A Consultation</button>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Header;
-
+export default withRouter(Header);
