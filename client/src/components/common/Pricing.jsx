6ab387942c8552655ef42219f4da2fb9f0df7ae8
Chris <chriskirbydev@gmail.com>
2020-08-19 13:52:58 +1200

    remove linked components

--- client/src/components/common/Pricing.jsx
+++ client/src/components/common/Pricing.jsx
@@ -1,57 +1,66 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
-function Pricing(){
-    return(
-        <div className="pricing-section" id="pricing">
-            <div className="pricing-title">
-                <h1>Pricing</h1>
-            </div>
-        <div className="pricing-table-wrapper">
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <div className="card bg-1">
-                            <label>Cupcakes</label>
-                            <h3>from <sup>£</sup>10</h3>
-                            <ul>
-                                <li>Available in boxes of 4, 6 and 12</li>
-                                <li>Custom designs are also available</li>
-                                <li>Get in touch with your ideas!</li>
-                            </ul>
-                            <Link to="/form"><a href="#" className="btn">Book Now</a></Link>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card bg-2">
-                            <label>Celebration Cake</label>
-                            <h3>from <sup>£</sup>30</h3>
-                            <ul>
-                                <li>Sizes available: 4", 6", 8", 10" and 12"</li>
-                                <li>For all occasions, bespoke cakes</li>
-                                <li>especially for you!</li>
-                            </ul>
-                            <Link to="/form"><a href="#" className="btn">Book Now</a></Link>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card bg-3">
-                            <label>Wedding Cake</label>
-                            <h3>On Consultation</h3>
-                            <ul>
-                                <li>Single tier, 2 Tier or 3 Tier</li>
-                                <li>(available sizes 4", 6", 8", 10" and 12")</li>
-                                <li>Get in touch for a cake consultation!</li>
-                            </ul>
-                            <Link to="/form"><a href="#" className="btn">Book Now</a></Link>
-                        </div>
-                    </div>
+function Pricing() {
+  return (
+    <div className="pricing-section" id="pricing">
+      <div className="pricing-title">
+        <h1>Pricing</h1>
+      </div>
+      <div className="pricing-table-wrapper">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <div className="card bg-1">
+                <label>Cupcakes</label>
+                <h3>
+                  from <sup>£</sup>10
+                </h3>
+                <ul>
+                  <li>Available in boxes of 4, 6 and 12</li>
+                  <li>Custom designs are also available</li>
+                  <li>Get in touch with your ideas!</li>
+                </ul>
+                <div onClick={() => this.props.history.push("/form")}>
+                  <p className="btn">Book Now</p>
                 </div>
+              </div>
             </div>
+            <div className="col">
+              <div className="card bg-2">
+                <label>Celebration Cake</label>
+                <h3>
+                  from <sup>£</sup>30
+                </h3>
+                <ul>
+                  <li>Sizes available: 4", 6", 8", 10" and 12"</li>
+                  <li>For all occasions, bespoke cakes</li>
+                  <li>especially for you!</li>
+                </ul>
+                <div onClick={() => this.props.history.push("/form")}>
+                  <p className="btn">Book Now</p>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card bg-3">
+                <label>Wedding Cake</label>
+                <h3>On Consultation</h3>
+                <ul>
+                  <li>Single tier, 2 Tier or 3 Tier</li>
+                  <li>(available sizes 4", 6", 8", 10" and 12")</li>
+                  <li>Get in touch for a cake consultation!</li>
+                </ul>
+                <div onClick={() => this.props.history.push("/form")}>
+                  <p className="btn">Book Now</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Pricing;
-
+export default withRouter(Pricing);
